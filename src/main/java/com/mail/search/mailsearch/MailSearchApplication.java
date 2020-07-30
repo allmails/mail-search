484715c8c839676d78e514397ca46ecd83a107a8
satyadev20 <garikapatisrisatyadev@gmail.com>
2020-07-29 22:01:49 -0500

    Initial Commit

---
+++ src/main/java/com/mail/search/mailsearch/MailSearchApplication.java
@@ -0,0 +1,13 @@
+package com.mail.search.mailsearch;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MailSearchApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MailSearchApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/mail/search/mailsearch/MailSearchApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mail.search.mailsearch;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MailSearchApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
